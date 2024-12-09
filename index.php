7ec3f1fc7713892127c0ebc6ba2b6878ee63a962
Sunnyc0ve <toni.sannikov.03@gmail.com>
2024-12-09 20:10:22 +0500

    Добавил json

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+
+<?php include 'includes/header.php'; ?>
+<main>
+    <h2>Welcome to my website!</h2>
+    <p>This is the homepage.</p>
+</main>
+<?php include 'includes/footer.php'; ?>
