2d6bf6cf888ee4f61208e3c87f37adc74a05ea40
Sunnyc0ve <toni.sannikov.03@gmail.com>
2024-12-09 21:58:31 +0500

    Добавил хедер, следущий этап форматирование пхп файла

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <footer>
     <p>Подвал</p>
 </footer>
+<script scr="js/script.js"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,36 @@
+<?php include 'functions.php';?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Периметр ремонта</title>
+    <link rel="stylesheet" href="<?php echo 'css/style.css'; ?>">
+
+
 </head>
 <body>
 <header>
-    <h1>Шапка</h1>
+    <div class="container">
+        <div class="logo_container">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+                <div class="header_nav">
+                    <nav class="nav_container">
+                        <ul class="nav_menu">
+                            <li>Главная</li>
+                            <li>Проекты</li>
+                            <li>Команда</li>
+                            <li>Акция</li>
+                            <li>Услуги</li>
+                            <div class="header_contacts">
+                                <p class="header_phone">+7 (911) 929-77-13</p>
+                                <button class="btn">Оставить заявку</button>
+                            </div>
+                        </ul>
+                    </nav>
+                </div>
+    </div>
 </header>
