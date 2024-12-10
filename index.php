99791ec7cdc1f315d8247006446b4100a4d3d1ae
Sunnyc0ve <toni.sannikov.03@gmail.com>
2024-12-10 08:43:48 +0500

    Готовый главный экран

--- index.php
+++ index.php
@@ -7,11 +7,33 @@ ini_set('display_errors', 1);
 <?php include 'includes/header.php'; ?>
 
 <main>
-    <section class="full-width-bg">
+    <section class="main_section full-width-bg">
         <div class="container">
             <div class="main_container">
-
-
+                <div class="main_container-features">
+                    <h1 class="main_container-title">
+                        Безупречный по стилю<br>дизайн интерьера и реализация проекта под ключ.
+                    </h1>
+                    <div class="main_features">
+                        <p>Выгода до 15% за счет связки: дизайн+строители+подрядчики</p>
+                        <p>Твердые сроки сдачи, внесенные в договор</p>
+                    </div>
+                </div>
+                <div class="main_container-stats">
+                    <div class="main_stat main_stat1">
+                        <h3 class="main_title-h3"><span class="main_title-num">5</span><br> Лет нашей компании</h3>
+                    </div>
+                    <div class="main_stat main_stat2">
+                        <h3 class="main_title-h3"><span class="main_title-num">400+</span><br> Проектов реализовано</h3>
+                    </div>
+                    <div class="main_stat main_stat3">
+                        <h3 class="main_title-h3"><span class="main_title-num">200+</span><br> Отзывов на Яндекс картах и 2ГИС</h3>
+                    </div>
+                </div>
+                <button class="btn main_btn" type="button">Посмотреть отзывы</button>
+                <div class="main_video-container">
+                    <video src="videos/mainvid.mp4" class="main_video" autoplay muted loop aria-label="Видеопрезентация интерьера.">Ваш браузер не поддерживает видео.</video>
+                </div>
             </div>
         </div>
     </section>
